fa406a32d130664fcaa66556784d7417303fc06c
yar <ahmadyar228@gmail.com>
2022-04-26 14:23:19 +0500

    first round of compression algo completed

--- challenge-1/count_occurence.py
+++ challenge-1/count_occurence.py
@@ -10,13 +10,14 @@ def compress(input_string):
             counted_letter = letter
             print("changed letter: ", counted_letter)
             count = 1
-        # elif string_length == index + 1:
-        #     count += 1
-        #     compressed_string += f'{counted_letter}{count}'
-
         else:
             count += 1
             print(f"count of {counted_letter} is {count}")
+
+        if string_length == index + 1:
+            # End of string
+            compressed_string += f'{counted_letter}{count}'
+
     return compressed_string
 
 # assert compress('bbcceeee') == 'b2c2e4'
@@ -24,5 +25,5 @@ def compress(input_string):
 # test = 'aafggiII'
 # test1 = 'aaabbbcccaaa'
 # assert compress('aaabbbcccaaa') == 'a3b3c3a3'
-print(compress('bbcceeeeb'))
+print(compress('bbccbbhu'))
 
