8f7cac1e28ed59f88446386ab0d081723e295664
yar <ahmadyar228@gmail.com>
2022-04-28 15:44:11 +0500

    base template and registration form

--- challenge-2/test_project/settings.py
+++ challenge-2/test_project/settings.py
@@ -26,9 +26,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'django_registration',
     'test_app',
     'users',
+    'django_registration',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
