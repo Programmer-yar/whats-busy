e99de6a186dfea37f2ef8f617951478c53dffa27
yar <ahmadyar228@gmail.com>
2022-04-28 15:19:05 +0500

    postgres attached

--- challenge-2/test_project/settings.py
+++ challenge-2/test_project/settings.py
@@ -67,8 +67,12 @@ WSGI_APPLICATION = 'test_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'whats-busy',
+        'USER': 'postgres',
+        'PASSWORD': 'django-pg-5678',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
